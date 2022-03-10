faf0a5a9e9895fade9507502c71c25588d34305f
Victorspy-web <dicksonsmart0711@gmail.com>
2022-03-10 02:06:48 +0000

    Typo

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k#7=!436e+2x163jw7)l0n^-it0ie0n(enpj)f(71%(hgsc5kt'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
